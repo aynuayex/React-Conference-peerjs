dd44557b183ebd74873dd9303253d4540cd61eff
Ayne Abreham Alemayehu <aynuman19@gmail.com>
2024-11-13 18:33:44 +0300

    deploy: updated backend deployed url on the frontend2

--- client/src/ws.ts
+++ client/src/ws.ts
@@ -2,7 +2,7 @@ import socketIO from "socket.io-client";
 
 
 // const server = "https://react-conference-peerjs.onrender.com";
-const server = "https://react-conference-peerjs-backend.vercel.app/";
+const server = "https://react-conference-peerjs-backend.vercel.app";
 
 // const server = "http://localhost:8080";
 export const ws = socketIO(server);
